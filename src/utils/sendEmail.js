db73dae2565e54fee786fcc3ce8c8e8f3e0468d4
shrikant <shrikantbirajdar452@gmail.com>
2026-06-27 17:46:35 +0530

    Fix email: use port 465 with SSL

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -9,11 +9,16 @@ const createTransporter = () => {
     }
 
     return nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: { user, pass },
-        connectionTimeout: 15000,
-        greetingTimeout: 10000,
-        socketTimeout: 20000,
+        connectionTimeout: 30000,
+        greetingTimeout: 30000,
+        socketTimeout: 30000,
+        tls: {
+            rejectUnauthorized: false
+        }
     });
 };
 
