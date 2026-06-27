dbfc9a460ce3db7e02cbc15ab2bb68123a5a73f8
solapurgurukulam <solapurgurukulam@gmail.com>
2026-06-27 23:13:59 +0530

    Update generateSlug.js

--- src/utils/generateSlug.js
+++ src/utils/generateSlug.js
@@ -1,11 +1,26 @@
 const slugify = require('slugify');
 
 const generateSlug = (text) => {
-    return slugify(text, {
+    if (!text || typeof text !== 'string') {
+        return 'mantra-' + Date.now() + '-' + Math.random().toString(36).slice(2, 6);
+    }
+
+    // Try normal slugify (works for English/Roman text)
+    let slug = slugify(text, {
         lower: true,
         strict: true,
-        remove: /[*+~.()'"!:@]/g,
+        remove: /[*+~.()'\"!:@]/g,
     });
+
+    // Clean up stray dashes
+    slug = slug.replace(/-+/g, '-').replace(/^-|-$/g, '');
+
+    // If slug is empty (pure Sanskrit/Devanagari text), generate unique fallback
+    if (!slug) {
+        slug = 'mantra-' + Date.now() + '-' + Math.random().toString(36).slice(2, 6);
+    }
+
+    return slug;
 };
 
 module.exports = generateSlug;
